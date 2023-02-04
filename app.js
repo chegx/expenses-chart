8b779fd9f3b8d0405ce2ec2bc1a6102221ae45c6
chegx <chegsar@gmail.com>
2023-02-05 02:53:13 +0900

    Put the chart outside of async function

--- app.js
+++ app.js
@@ -1,123 +1,125 @@
 async function getData() {
   const response = await fetch("./data.json");
-  const myData = await response.json();
+  const data = await response.json();
+  return data;
+}
 
-  const xData = [];
-  const yData = [];
-  const barColors = [];
-  const hoverBarColors = [];
-  const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-  const mediaQuery = window.matchMedia('(min-width: 820px)'); //for changing font size and caret padding
+const myData = await getData();
+const xData = [];
+const yData = [];
+const barColors = [];
+const hoverBarColors = [];
+const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const mediaQuery = window.matchMedia('(min-width: 820px)'); //for changing font size and caret padding
 
-  for (let i = 0; i < 7; i++) {
-    xData.push(myData[i].day);
-    yData.push(myData[i].amount);
-    barColors.push("hsl(10, 79%, 65%)");   //color every bar soft red
-    hoverBarColors.push("hsla(10, 79%, 65%, 0.7)"); //color on hover every bar transparent red
-  }
+for (let i = 0; i < 7; i++) {
+  xData.push(myData[i].day);
+  yData.push(myData[i].amount);
+  barColors.push("hsl(10, 79%, 65%)");   //color every bar soft red
+  hoverBarColors.push("hsla(10, 79%, 65%, 0.7)"); //color on hover every bar transparent red
+}
 
-  //get the current day and convert it so the week starts on Monday  
-  const currentDate = new Date();
-  const currentDay = currentDate.getDay();
-  const todayIndex = (currentDay + 6) % 7;
+//get the current day and convert it so the week starts on Monday  
+const currentDate = new Date();
+const currentDay = currentDate.getDay();
+const todayIndex = (currentDay + 6) % 7;
 
-  barColors[todayIndex] = "hsl(186, 34%, 60%)";
-  hoverBarColors[todayIndex] = "hsla(186, 34%, 60%, 0.7)";
+barColors[todayIndex] = "hsl(186, 34%, 60%)";
+hoverBarColors[todayIndex] = "hsla(186, 34%, 60%, 0.7)";
 
-  //get the sum of all values and show it in the total
-  //add text with data for screen readers
-  var sum = 0;
-  var text = "";
-  for (let i = 0; i < 7; i++) {
-    sum += yData[i];
-    text += weekday[i] + " $" + yData[i] + " ";
-  }
-  document.getElementById("total").textContent = "$" + sum.toFixed(2);
-  document.getElementById("chartText").textContent = text;
+//get the sum of all values and show it in the total
+//add text with data for screen readers
+var sum = 0;
+var text = "";
+for (let i = 0; i < 7; i++) {
+  sum += yData[i];
+  text += weekday[i] + " $" + yData[i] + " ";
+}
+document.getElementById("total").textContent = "$" + sum.toFixed(2);
+document.getElementById("chartText").textContent = text;
 
-  //set defalt properties
-  Chart.defaults.borderColor = 'transparent'; //remove bottom line
-  Chart.defaults.font.family = 'DM Sans';
+//set defalt properties
+Chart.defaults.borderColor = 'transparent'; //remove bottom line
+Chart.defaults.font.family = 'DM Sans';
 
-  const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: xData,
-      datasets: [{
-        data: yData,
-        backgroundColor: barColors,
-        hoverBackgroundColor: hoverBarColors,
-        borderRadius: 4,
-        borderSkipped: false //round bottom corners
-      }]
-    },
-    options: {
-      plugins: {
-        title: {
-          display: false,
-        },
-        legend: {
-          display: false,
-        },
-        tooltip: {
-          xAlign: "center",
-          yAlign: "bottom",
-          backgroundColor: "hsl(25, 47%, 15%)",
-          caretSize: 0,
-          caretPadding: 9,
-          displayColors: false,
-          bodyFont: {
-            size: () => {
-              if (mediaQuery.matches) {
-                return 18
-              } else {
-                return 14
-              }
-            },
-            weight: 700
-          },
-          padding: () => {
-            if (mediaQuery.matches) {
-              return 9
-            } else {
-              return 4
-            }
-          },
-          callbacks: {
-            title: () => null,
-            label: (context) => {
-              return '$' + context.parsed.y
-            }
-          }
-        }
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: xData,
+    datasets: [{
+      data: yData,
+      backgroundColor: barColors,
+      hoverBackgroundColor: hoverBarColors,
+      borderRadius: 4,
+      borderSkipped: false //round bottom corners
+    }]
+  },
+  options: {
+    plugins: {
+      title: {
+        display: false,
       },
-      scales: {
-        x: {
-          ticks: {
-            color: "hsl(28, 10%, 53%)",
-            font: {
-              size: () => {
-                if (mediaQuery.matches) {
-                  return 15
-                } else {
-                  return 12
-                }
-              }
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        xAlign: "center",
+        yAlign: "bottom",
+        backgroundColor: "hsl(25, 47%, 15%)",
+        caretSize: 0,
+        caretPadding: 9,
+        displayColors: false,
+        bodyFont: {
+          size: () => {
+            if (mediaQuery.matches) {
+              return 18
+            } else {
+              return 14
             }
           },
-          grid: {
-            display: false
+          weight: 700
+        },
+        padding: () => {
+          if (mediaQuery.matches) {
+            return 9
+          } else {
+            return 4
           }
         },
-        y: {
-          display: false,
-          grace: "50%" //add space above bars
+        callbacks: {
+          title: () => null,
+          label: (context) => {
+            return '$' + context.parsed.y
+          }
         }
       }
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: "hsl(28, 10%, 53%)",
+          font: {
+            size: () => {
+              if (mediaQuery.matches) {
+                return 15
+              } else {
+                return 12
+              }
+            }
+          }
+        },
+        grid: {
+          display: false
+        }
+      },
+      y: {
+        display: false,
+        grace: "50%" //add space above bars
+      }
     }
-  });
-}
+  }
+});
 
 window.addEventListener("load", getData);
